ed9fc4176361adec4e25d5586375fa886eeb81f4
Allan Hassan <302795718@student.rocmondriaan.nl>
2022-01-18 00:47:44 +0100

    bugs verbeterd

--- Templates/defaults/pictures.php
+++ Templates/defaults/pictures.php
@@ -1,5 +1,17 @@
 <figure>
-    <img class="banner-img img-fluid" src='/img/healthone-wide.png' />
+    <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <img src="/img/healthone-wide1.jpg" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="/img/healthone-wide2.png" class="d-block w-100" alt="...">
+            </div>
+            <div class="carousel-item">
+                <img src="/img/healthone-wide3.png" class="d-block w-100" alt="...">
+            </div>
+        </div>
+    </div>
 </figure>
 
 
